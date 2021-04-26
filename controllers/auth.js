aeb7c2c5a02093ce6fe16434ad7d2d1b6ea3fd8a
joawa10 <joawa10@yahoo.com>
2021-04-26 21:31:29 +0800

    add method for login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const { validationResult } = require('express-validator');
 
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -25,10 +26,53 @@ exports.signup = async (req, res, next) => {
     const result = await User.save(userDetails);
 
     res.status(201).json({ message: "User registered!" })
-  } catch (error) {
+  } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
     }
     next(err)
   }
-}
+};
+
+exports.login = async (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  try {
+    const user = await User.find(email); //find the row of the user
+
+    //check if the email exist
+    if (user[0].length !== 1) {
+      const error = new Error('A user with this email could not be found.');
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const storedUser = user[0][0];
+
+    //compare input password to the hashed password in the db
+    const isEqual = await bcrypt.compare(password, storedUser.password);
+
+    if (!isEqual) {
+      const error = new Error('Wrong password!');
+      error.statusCode = 401;
+      throw error;
+    }
+
+    //give token to user
+    const token  = jwt.sign(
+      {
+        email: storedUser.email,
+        user_id: storedUser.user_id
+      },
+      'secretfortoken',
+      { expiresIn: '1h' }
+    );
+    res.status(200).json({ token: token, user_id: storedUser.user_id });
+
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err)
+  }  
+};
