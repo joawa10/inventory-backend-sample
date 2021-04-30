6b16cd9eea4f9faf0108d7d877155ce142291fbc
joawa10 <joawa10@yahoo.com>
2021-04-30 14:30:35 +0800

    create middleware to authenticate routes

--- routes/customers.js
+++ routes/customers.js
@@ -3,7 +3,17 @@ const express = require('express');
 
 const customersController = require('../controllers/customers');
 
+// const auth = require('../middleware/auth');
+
 const router = express.Router();
+
+// router.get('/', auth, customersController.getAllCustomers);
+
+// router.post('/', auth, customersController.postCustomers);
+
+// router.put('/', auth, customersController.putCustomers);
+
+// router.delete('/:customers_id', auth, customersController.deleteCustomers);
 
 router.get('/', customersController.getAllCustomers);
 
